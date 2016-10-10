bc1822925f2403886fff65b5d7064c4e84480db5
Tom Coleman <tom@thesnail.org>
2016-10-10 15:52:15 +1100

    Changed the module namespace for proto stuff

--- lib/optics-agent/reporting/send-report.rb
+++ lib/optics-agent/reporting/send-report.rb
@@ -1,47 +1,49 @@
-require 'optics-agent/proto/reports_pb'
+require 'apollo/optics/proto/reports_pb'
 require_relative './send-message'
 
 module OpticsAgent::Reporting
+  include Apollo::Optics::Proto
+
   def self.send_report
     # just a made up query for now
     queryKey = '{ posts { title } }'
 
     # basic stuff about the trace
-    report = OpticsAgent::Proto::StatsReport.new({
-      header: OpticsAgent::Proto::ReportHeader.new({
+    report = StatsReport.new({
+      header: ReportHeader.new({
         agent_version: '1'
       }),
-      start_time: OpticsAgent::Proto::Timestamp.new({
+      start_time: Timestamp.new({
         seconds: Time.now.to_i / 1000,
         nanos: 0
       }),
-      end_time: OpticsAgent::Proto::Timestamp.new({
+      end_time: Timestamp.new({
         seconds: Time.now.to_i / 1000 + 5,
         nanos: 0
       })
     })
 
     # now query stats
-    stats_per_signature = OpticsAgent::Proto::StatsPerSignature.new
-    stats_per_signature.per_type << OpticsAgent::Proto::TypeStat.new({
+    stats_per_signature = StatsPerSignature.new
+    stats_per_signature.per_type << TypeStat.new({
       name: 'Query',
       field: [
-        OpticsAgent::Proto::FieldStat.new({
+        FieldStat.new({
           name: 'posts',
           returnType: '[Post]'
         })
       ]
     })
-    stats_per_signature.per_type << OpticsAgent::Proto::TypeStat.new({
+    stats_per_signature.per_type << TypeStat.new({
       name: 'Post',
       field: [
-        OpticsAgent::Proto::FieldStat.new({
+        FieldStat.new({
           name: 'title',
           returnType: 'String'
         })
       ]
     })
-    stats_per_signature.per_type << OpticsAgent::Proto::TypeStat.new({
+    stats_per_signature.per_type << TypeStat.new({
       name: 'String'
     })
     report.per_signature[queryKey] = stats_per_signature
