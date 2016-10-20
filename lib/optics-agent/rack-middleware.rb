0079919b0d72c054b3b8bac7f61d309ff8331d83
Marc-André Giroux <mgiroux0@gmail.com>
2016-10-20 09:48:43 -0400

    rm catch all rescue from middleware

--- lib/optics-agent/rack-middleware.rb
+++ lib/optics-agent/rack-middleware.rb
@@ -8,36 +8,31 @@ module OpticsAgent
     end
 
     def call(env)
-      begin
-        start_time = Time.now
+      start_time = Time.now
 
-        # XXX: figure out a way to pass this in here
-        agent = OpticsAgent::Agent.instance
-        query = OpticsAgent::Reporting::Query.new
+      # XXX: figure out a way to pass this in here
+      agent = OpticsAgent::Agent.instance
+      query = OpticsAgent::Reporting::Query.new
 
-        # Attach so resolver middleware can access
-        env[:optics_agent] = {
-          agent: agent,
-          query: query
-        }
-        env[:optics_agent].define_singleton_method(:with_document) do |document|
-          self[:query].document = document
-          self
-        end
-
-        result = @app.call(env)
-
-        # XXX: this approach means if the user forgets to call with_document
-        # we just never log queries. Can we detect if the request is a graphql one?
-        if (query.document)
-          agent.add_query(query, env, start_time, Time.now)
-        end
-
-        result
-      rescue Exception => e
-        puts "Rack Middleware Error: #{e}"
-        puts e.backtrace
+      # Attach so resolver middleware can access
+      env[:optics_agent] = {
+        agent: agent,
+        query: query
+      }
+      env[:optics_agent].define_singleton_method(:with_document) do |document|
+        self[:query].document = document
+        self
       end
+
+      result = @app.call(env)
+
+      # XXX: this approach means if the user forgets to call with_document
+      # we just never log queries. Can we detect if the request is a graphql one?
+      if (query.document)
+        agent.add_query(query, env, start_time, Time.now)
+      end
+
+      result
     end
   end
 end
